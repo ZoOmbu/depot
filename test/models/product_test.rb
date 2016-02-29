d59842aceb735ecfaeeabf64c7f0933acd0f2186
ZoOmbu <valerii.bondarenko@nure.ua>
2016-02-29 12:54:48 +0200

    Создание корзины и добавление в корзину

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test "product is not valid without a unique title" do
 # если у товара нет уникального названия, то он недопустим
     product = Product.new(title: products(:ruby).title,
